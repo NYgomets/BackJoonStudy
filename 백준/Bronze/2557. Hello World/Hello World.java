fa7143f6802036840388f039a1ed86731c45c722
faso2 <126853427+faso2@users.noreply.github.com>
2023-11-10 22:32:29 +0900

    [Bronze V] Title: Hello World, Time: 108 ms, Memory: 14032 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,12 @@
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.print("Hello World!");
+    }
+}
