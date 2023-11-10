4ae96b73dadcb829610d2f9fb48ef30b0994ed38
faso2 <126853427+faso2@users.noreply.github.com>
2023-11-11 05:45:45 +0900

    [Bronze V] Title: A+B - 7, Time: 148 ms, Memory: 16028 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int test = Integer.parseInt(br.readLine());
+
+        for (int i=1; i<=test; i++) {
+            st = new StringTokenizer(br.readLine()," ");
+            bw.write("Case #"+ i + ": ");
+            bw.write(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())+ "\n");
+        }
+        br.close();
+
+        bw.flush();
+        bw.close();
+
+        bw.close();
+    }
+}
