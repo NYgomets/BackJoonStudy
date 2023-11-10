5163664a3dd15f504334df2b49b7631a80425b6f
faso2 <126853427+faso2@users.noreply.github.com>
2023-11-10 22:48:56 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,17 @@
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+       String A = scanner.next();
+        System.out.println(A+"??!");
+    }
+}
