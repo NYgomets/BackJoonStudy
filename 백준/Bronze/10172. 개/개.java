492deb66fcf243d3bf967c920605325a41bcc605
faso2 <126853427+faso2@users.noreply.github.com>
2023-11-10 23:41:22 +0900

    [Bronze V] Title: 개, Time: 124 ms, Memory: 13964 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        bw.write("|\\_/|");
+        bw.newLine();	// 줄 구분자
+
+        bw.write("|q p|   /}");
+        bw.newLine();
+
+        bw.write("( 0 )\"\"\"\\");
+        bw.newLine();
+
+        bw.write("|\"^\"`    |");
+        bw.newLine();
+
+        bw.write("||_/=\\\\__|");
+
+        bw.flush();
+        bw.close();
+
+    }
+}
