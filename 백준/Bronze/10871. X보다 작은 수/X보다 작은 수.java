b457abc3a62c68c101499e6370e0cfb9787a830f
faso2 <126853427+faso2@users.noreply.github.com>
2023-11-12 21:59:36 +0900

    [Bronze V] Title: X보다 작은 수, Time: 524 ms, Memory: 30216 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int findNum = scanner.nextInt();
+        int[] a = new int[num];
+        for (int i=0; i<num; i++) {
+            a[i] = scanner.nextInt();
+        }
+        for (int i=0; i<num; i++) {
+            if (a[i] < findNum) {
+                System.out.println(a[i]);
+            }
+        }
+    }
+}
