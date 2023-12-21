182b308ab3c2f2f0a9b0622839676fb7b700d98a
faso2 <126853427+faso2@users.noreply.github.com>
2023-12-22 01:17:51 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 232 ms, Memory: 17964 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+       Scanner s = new Scanner(System.in);
+
+       while (s.hasNext()) {
+           System.out.println(s.nextLine());
+       }
+    }
+}
