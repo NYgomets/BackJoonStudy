c09f2c2a476f924113be047b549206a86909057a
faso2 <126853427+faso2@users.noreply.github.com>
2023-12-29 14:44:59 +0900

    [Bronze I] Title: 소인수분해, Time: 124 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        for (int i = 2; i * i <= n; i++) {
+            while (n % i == 0) {
+                System.out.println(i);
+                n /= i;
+            }
+        }
+
+        if (n > 1) {
+            System.out.println(n);
+        }
+    }
+}
