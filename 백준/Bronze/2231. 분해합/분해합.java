c46cf6fef6c4004fd18bc0bcb80fa93256aa9036
faso2 <126853427+faso2@users.noreply.github.com>
2024-01-01 09:38:10 +0900

    [Bronze II] Title: 분해합, Time: 124 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,14 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner s = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String num = s.next();
+        String num = st.nextToken();
         int Num = Integer.parseInt(num);
         char c = num.charAt(0);
         int i = ((int) (c - '0')) + (9 * (num.length() - 1));
@@ -26,7 +26,7 @@ public class Main {
             }
 
             sum += checkNum;
-            
+
             if (sum == Num) {
                 System.out.println(checkNum);
                 return;
