2b1248f0156591854e5351393a76fd2d19fb2518
faso2 <126853427+faso2@users.noreply.github.com>
2024-01-01 09:36:20 +0900

    [Bronze II] Title: 분해합, Time: 200 ms, Memory: 17604 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,7 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
+import java.math.BigInteger;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -9,23 +10,23 @@ public class Main {
 
         String num = s.next();
         int Num = Integer.parseInt(num);
+        char c = num.charAt(0);
+        int i = ((int) (c - '0')) + (9 * (num.length() - 1));
 
-        int i = Character.getNumericValue(num.charAt(0)) + 9 * (num.length() - 1);
         int checkNum = Num - i;
-
         int sum = 0;
 
         while (checkNum <= Num) {
             String checknum = String.valueOf(checkNum);
             sum = 0;
 
-            for (int k = 0; k < checknum.length(); k++) {
-                int a = Character.getNumericValue(checknum.charAt(k));
+            for (int k=0; k<checknum.length(); k++) {
+                int a = (int) checknum.charAt(k) - '0';
                 sum += a;
             }
 
             sum += checkNum;
-
+            
             if (sum == Num) {
                 System.out.println(checkNum);
                 return;
@@ -33,7 +34,6 @@ public class Main {
                 checkNum++;
             }
         }
-
-        System.out.println(0); // 조건을 만족하는 숫자를 찾을 수 없을 경우 0 출력
+            System.out.println(0);
     }
 }
