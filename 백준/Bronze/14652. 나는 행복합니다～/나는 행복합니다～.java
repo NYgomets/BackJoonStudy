ea1e46df5cac090f3993c08ce645a9e3376e6848
faso2 <126853427+faso2@users.noreply.github.com>
2024-01-30 18:16:37 +0900

    [Bronze IV] Title: 나는 행복합니다~, Time: 1584 ms, Memory: 17868 KB -BaekjoonHub

---
+++ 백준/Bronze/14652. 나는 행복합니다～/나는 행복합니다～.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int row = Integer.parseInt(st.nextToken());
+        int column = Integer.parseInt(st.nextToken());
+        int mysit = Integer.parseInt(st.nextToken());
+
+        int count = 0;
+
+        for(int i=0;i<row;i++) {
+            for(int j=0;j<column;j++) {
+                if(mysit==count) {
+                    bw.write(i+" "+j);
+                    bw.flush();
+                }
+                count++;
+            }
+        }
+        bw.close();
+    }
+}
