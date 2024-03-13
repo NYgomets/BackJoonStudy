79b9c2f394a6b8195b71c1ef66bdeb276be48364
NYgomets <126853427+NYgomets@users.noreply.github.com>
2024-03-13 20:03:30 +0900

    [Silver I] Title: 절댓값 힙, Time: 660 ms, Memory: 29472 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,46 +1,30 @@
 import java.io.*;
 import java.util.*;
 
-class Check {
-    int a_Abs;
-    int a;
-
-    public Check(int a_Abs, int a) {
-        this.a_Abs = a_Abs;
-        this.a = a;
-    }
-}
-
-class CheckComparator implements Comparator<Check> {
-
-    @Override
-    public int compare(Check o1, Check o2) {
-        if (o1.a_Abs == o2.a_Abs) {
-            return o1.a - o2.a;
-        } else {
-            return o1.a_Abs - o2.a_Abs;
-        }
-    }
-}
-
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner s = new Scanner(System.in);
-
-        PriorityQueue<Check> pq = new PriorityQueue<>(1, new CheckComparator());
-
-        int N = s.nextInt();
-        for (int i=0; i<N; i++) {
-            int x = s.nextInt();
-            if (x != 0) {
-                pq.add(new Check(Math.abs(x), x));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1,o2) -> {
+            int first_abs = Math.abs(o1);
+            int second_abs = Math.abs(o2);
+            if (first_abs == second_abs) {
+                return o1 - o2;
             } else {
-                if (!pq.isEmpty()) {
-                    Check p = pq.poll();
-                    System.out.println(p.a);
+                return first_abs - second_abs;
+            }
+        });
+
+        for (int i=0; i<N; i++) {
+            int request = Integer.parseInt(br.readLine());
+            if (request == 0) {
+                if (pq.isEmpty()) {
+                    System.out.println("0");
                 } else {
-                    System.out.println(0);
+                    System.out.println(pq.poll());
                 }
+            } else {
+                pq.add(request);
             }
         }
     }
