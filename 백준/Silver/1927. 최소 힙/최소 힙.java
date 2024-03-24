cad60850400bc014b9077ae8f96b29cd87cf9c3a
NYgomets <126853427+NYgomets@users.noreply.github.com>
2024-03-24 22:09:08 +0900

    [Silver II] Title: 최소 힙, Time: 1188 ms, Memory: 30596 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.math.BigDecimal;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>((o1, o2) -> {
+            return o1-o2;
+        });
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i=0; i<N; i++) {
+            int check = Integer.parseInt(br.readLine());
+            if (check == 0) {
+                if (!priorityQueue.isEmpty()) {
+                    System.out.println(priorityQueue.poll());
+                } else {
+                    System.out.println(0);
+                }
+            } else {
+                priorityQueue.add(check);
+            }
+        }
+    }
+}
