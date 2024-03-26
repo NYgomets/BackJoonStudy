4586ba00ae34999ffe48f860d461de3b1e02448a
NYgomets <126853427+NYgomets@users.noreply.github.com>
2024-03-26 18:33:30 +0900

    [Silver V] Title: K번째 수, Time: 1944 ms, Memory: 378932 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.math.BigDecimal;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ", 2);
+        int N = Integer.parseInt(split[0]);
+        int K = Integer.parseInt(split[1]);
+
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[K-1]);
+    }
+}
