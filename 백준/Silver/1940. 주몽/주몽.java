58c4968377d16f0c1c87e03b7db905117876d498
NYgomets <126853427+NYgomets@users.noreply.github.com>
2024-10-23 10:40:25 +0900

    [Silver IV] Title: 주몽, Time: 172 ms, Memory: 16404 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,37 +1,32 @@
 import java.io.*;
+import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner s = new Scanner(System.in);
-        int N= s.nextInt();
-        int M = s.nextInt();
-        int[] checkArray = new int[N+1];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
 
-        for (int i=1; i<=N; i++) {
-            checkArray[i] = s.nextInt();
+        for (int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(checkArray);
+        Arrays.sort(arr);
 
         int count = 0;
-
-        int sum = 0;
-        int start_index = 1;
-        int end_index = N;
-
-        while (start_index < end_index) {
-            sum = checkArray[start_index] + checkArray[end_index];
-            if (sum < M) {
-                sum = 0;
-                start_index++;
-            } else if (sum > M) {
-                sum = 0;
-                end_index--;
+        int start = 0;
+        int end = N-1;
+        while (end > start) {
+            if (M > (arr[start]+arr[end])) {
+                start++;
+            } else if (M < (arr[start]+arr[end])) {
+                end--;
             } else {
                 count++;
-                start_index++;
-                end_index--;
+                start++;
             }
         }
 
