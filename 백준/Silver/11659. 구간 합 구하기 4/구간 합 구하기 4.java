b74d67e15c11b7d33e2b58f31ea8b64c2f45b705
NYgomets <126853427+NYgomets@users.noreply.github.com>
2024-10-23 09:35:13 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1136 ms, Memory: 59524 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,33 +1,32 @@
 import java.io.*;
+import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner s = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String givenNum = br.readLine();
+        StringTokenizer st = new StringTokenizer(givenNum);
+        int N = Integer.parseInt(String.valueOf(st.nextToken()));
+        int M = Integer.parseInt(String.valueOf(st.nextToken()));
 
-        int N = s.nextInt();
-        int M = s.nextInt();
-
-        int[] nArray = new int[N+1];
-
-        for (int i=1; i<=N; i++) {
-            nArray[i] = s.nextInt();
+        int[] arr = new int[N+1];
+        int[] sum = new int[N+1];
+        String number = br.readLine();
+        st = new StringTokenizer(number);
+        arr[1] = Integer.parseInt(String.valueOf(st.nextToken()));
+        sum[1] = arr[1];
+        for (int i=2; st.hasMoreTokens(); i++) {
+            arr[i] = Integer.parseInt(String.valueOf(st.nextToken()));
+            sum[i] = sum[i-1] + arr[i];
         }
 
-        long[] sumArray = new long[N+1];
-        sumArray[1] = nArray[1];
-
-        for (int i=2; i<nArray.length; i++) {
-            sumArray[i] = sumArray[i-1] + nArray[i];
-        }
-
-        for (int k=0; k<M; k++) {
-            int i = s.nextInt();
-            int j = s.nextInt();
-
-            long check = sumArray[j] - sumArray[i-1];
-
-            System.out.println(check);
+        for (int j=0; j<M; j++) {
+            String value = br.readLine();
+            st = new StringTokenizer(value);
+            int a = Integer.parseInt(String.valueOf(st.nextToken()));
+            int b = Integer.parseInt(String.valueOf(st.nextToken()));
+            System.out.println(sum[b] - sum[a-1]);
         }
     }
 }
