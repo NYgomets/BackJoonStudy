5fb6cbd188940798f12a347b3fb7a3bd61a67ceb
NYgomets <126853427+NYgomets@users.noreply.github.com>
2024-10-25 20:07:41 +0900

    [Silver I] Title: 절댓값 힙, Time: 472 ms, Memory: 26672 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,30 +1,34 @@
 import java.io.*;
+import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> pq = new PriorityQueue<>((o1,o2) -> {
-            int first_abs = Math.abs(o1);
-            int second_abs = Math.abs(o2);
-            if (first_abs == second_abs) {
-                return o1 - o2;
-            } else {
-                return first_abs - second_abs;
+        Queue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                int a = Math.abs(o1);
+                int b = Math.abs(o2);
+                if (a == b) {
+                    return o1 - o2;
+                } else {
+                    return a - b;
+                }
             }
         });
 
         for (int i=0; i<N; i++) {
-            int request = Integer.parseInt(br.readLine());
-            if (request == 0) {
-                if (pq.isEmpty()) {
-                    System.out.println("0");
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                if (queue.isEmpty()) {
+                    System.out.println('0');
                 } else {
-                    System.out.println(pq.poll());
+                    System.out.println(queue.poll());
                 }
             } else {
-                pq.add(request);
+                queue.add(num);
             }
         }
     }
