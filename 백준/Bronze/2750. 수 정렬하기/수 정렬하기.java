5515df147d7086f30679e180955cae8650bec549
NYgomets <126853427+NYgomets@users.noreply.github.com>
2024-10-25 20:39:16 +0900

    [Bronze II] Title: 수 정렬하기, Time: 144 ms, Memory: 14904 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -5,18 +5,17 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
-        int[] checkArray = new int[N];
+        int[] arr = new int[N];
 
         for (int i=0; i<N; i++) {
-            checkArray[i] = Integer.parseInt(br.readLine());
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
-        Arrays.sort(checkArray);
+        Arrays.sort(arr);
 
-        for (int i=0; i<N; i++) {
-            System.out.println(checkArray[i]);
+        for (int i : arr) {
+            System.out.println(i);
         }
     }
 }
