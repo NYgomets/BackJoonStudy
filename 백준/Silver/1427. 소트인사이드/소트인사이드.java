d0d41692ea67036feebcb325354d6ede59017501
NYgomets <126853427+NYgomets@users.noreply.github.com>
2024-10-25 21:15:54 +0900

    [Silver V] Title: 소트인사이드, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,19 +1,15 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.math.BigDecimal;
+import java.io.*;
+import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String N = String.valueOf(br.readLine());
-        char[] charArray = N.toCharArray();
-
+        char[] charArray = br.readLine().toCharArray();
         Arrays.sort(charArray);
-
         StringBuilder sb = new StringBuilder();
-        System.out.println(sb.append(charArray).reverse());
+        sb.append(charArray).reverse();
+
+        System.out.print(sb);
     }
 }
