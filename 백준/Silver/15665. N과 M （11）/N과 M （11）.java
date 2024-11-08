6a204be10b694fecb236b9a53806d4e03b8616ea
NYgomets <126853427+NYgomets@users.noreply.github.com>
2024-11-08 10:24:27 +0900

    [Silver II] Title: N과 M (11), Time: 420 ms, Memory: 50724 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int m;
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        arr = new int[n];
+        for (int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        StringBuilder sb = new StringBuilder();
+        StringBuilder result = new StringBuilder();
+        back(0, sb, result, 0);
+
+        System.out.println(result);
+    }
+
+    private static void back(int depth, StringBuilder sb, StringBuilder result, int startUsed) {
+        if (depth == m) {
+            result.append(sb).append("\n");
+            return;
+        }
+
+        startUsed = -1;
+        for (int i=0; i<n; i++) {
+            if (arr[i] != startUsed) {
+                startUsed = arr[i];
+                sb.append(arr[i]).append(" ");
+                back(depth+1, sb, result, startUsed);
+                sb.delete(sb.length()-(String.valueOf(arr[i]).length()+1), sb.length());
+            }
+        }
+    }
+}
