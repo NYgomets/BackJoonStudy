ea275a7a180deba7488043db25bb49528100c1e0
NYgomets <126853427+NYgomets@users.noreply.github.com>
2024-11-18 18:43:09 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 2632 ms, Memory: 325252 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -0,0 +1,94 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int m;
+    static int[] arr;
+    static Node[] edgeList;
+    static class Node {
+        int start;
+        int end;
+        int weight;
+
+        public Node(int start, int end, int weight) {
+            this.start = start;
+            this.end = end;
+            this.weight = weight;
+        }
+
+        public int getStart() {
+            return start;
+        }
+
+        public int getEnd() {
+            return end;
+        }
+
+        public int getWeight() {
+            return weight;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n+1];
+        for (int i=1; i<=n; i++) {
+            arr[i] = i;
+        }
+
+        edgeList = new Node[m];
+        for (int i=0; i<m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            edgeList[i] = new Node(a, b, c);
+        }
+
+        Arrays.sort(edgeList, ((o1, o2) -> Integer.compare(o1.getWeight(), o2.getWeight())));
+
+        int result = 0;
+        int max = 0;
+        for (int i=0; i<m; i++) {
+            int start = edgeList[i].getStart();
+            int end = edgeList[i].getEnd();
+
+            if (find(start) == find(end)) {
+                continue;
+            }
+
+            union(start, end);
+            result += edgeList[i].getWeight();
+            if (max < edgeList[i].getWeight()) {
+                max = edgeList[i].getWeight();
+            }
+        }
+
+        System.out.println(result - max);
+    }
+
+    private static void union(int start, int end) {
+        int first = find(start);
+        int second = find(end);
+
+        if (first != second) {
+            if (first > second) {
+                arr[first] = second;
+            } else {
+                arr[second] = first;
+            }
+        }
+    }
+
+    private static int find(int start) {
+        if (arr[start] != start) {
+            arr[start] = find(arr[start]);
+        }
+
+        return arr[start];
+    }
+}
