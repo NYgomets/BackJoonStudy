d32899db4ff19a8c72a5f81477bb5315520986a7
NYgomets <126853427+NYgomets@users.noreply.github.com>
2024-12-18 13:31:12 +0900

    [Silver III] Title: 2×n 타일링, Time: 104 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -6,18 +6,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        long prevF = 1;
-        long prevS = 2;
-        for (int i=3; i<=n; i++) {
-            long temp = (prevF+prevS)%10007;
-            prevF = prevS;
-            prevS = temp;
+        int[] arr = new int[n+1];
+        arr[0]=1;
+        arr[1]=1;
+        for (int i=2; i<=n; i++) {
+            arr[i] = (arr[i-1]+arr[i-2])%10007;
         }
-        
-        if (n==1) {
-            System.out.println(prevF);
-        } else {
-            System.out.println(prevS);
-        }
+
+        System.out.println(arr[n]);
     }
 }
