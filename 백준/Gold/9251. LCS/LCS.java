50941bf2864aa55fdaf483a9cbaa78d979b95fcc
NYgomets <126853427+NYgomets@users.noreply.github.com>
2024-12-23 12:38:28 +0900

    [Gold V] Title: LCS, Time: 224 ms, Memory: 28604 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static String first;
+    static String second;
+    static Integer[][] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        first = br.readLine();
+        second = br.readLine();
+
+        dp = new Integer[first.length()][second.length()];
+        int result = 0;
+        for (int i=0; i<first.length(); i++) {
+            result = Math.max(result, find(i, 0));
+        }
+
+        System.out.println(result);
+    }
+
+    private static int find(int currentF, int currentS) {
+        if (currentF>=first.length() || currentS>=second.length()) {
+            return 0;
+        }
+
+        if (dp[currentF][currentS] != null) {
+            return dp[currentF][currentS];
+        }
+
+        int result = 0;
+        if (first.charAt(currentF)==second.charAt(currentS)) {
+            result = 1+find(currentF+1, currentS+1);
+        } else {
+            result = Math.max(find(currentF+1, currentS), find(currentF, currentS+1));
+        }
+
+        dp[currentF][currentS] = result;
+
+        return result;
+    }
+}
