566936b93895a4408383a077ba6b9c36021d2592
NYgomets <126853427+NYgomets@users.noreply.github.com>
2024-12-30 18:24:39 +0900

    [Silver I] Title: 1, 2, 3 더하기 5, Time: 188 ms, Memory: 20944 KB -BaekjoonHub

---
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int mod = 1000000009;
+    static long[][] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        int[] q = new int[t];
+        int maxN = 0;
+        for (int i=0; i<t; i++) {
+            int a = Integer.parseInt(br.readLine());
+            q[i] = a;
+            maxN = Math.max(maxN, a);
+        }
+
+        dp = new long[maxN+1][4];
+        dp[1][1] = 1;
+        dp[1][2] = 0;
+        dp[1][3] = 0;
+
+        if (maxN>=2) {
+            dp[2][1] = 0;
+            dp[2][2] = 1;
+            dp[2][3] = 0;
+        }
+        if (maxN>=3) {
+            dp[3][1] = 1;
+            dp[3][2] = 1;
+            dp[3][3] = 1;
+        }
+
+        for (int i=4; i<=maxN; i++) {
+            dp[i][1] = (dp[i-1][2]+dp[i-1][3])%mod;
+            dp[i][2] = (dp[i-2][1]+dp[i-2][3])%mod;
+            dp[i][3] = (dp[i-3][1]+dp[i-3][2])%mod;
+        }
+
+        for (int i : q) {
+            System.out.println((dp[i][1]+dp[i][2]+dp[i][3])%mod);
+        }
+    }
+}
