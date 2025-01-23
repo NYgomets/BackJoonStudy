2c5ab062a9c16d0757b1b48bb189650576cb7e47
NYgomets <126853427+NYgomets@users.noreply.github.com>
2025-01-23 19:45:51 +0900

    [level 1] Title: 약수의 합, Time: 0.04 ms, Memory: 70.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        int sum = 0;
+        for (int i=1; i<=Math.sqrt(n); i++) {
+            if (n%i == 0) {
+                sum += i;
+                if (i == n/i) {
+                    continue;
+                }
+                sum += n/i;
+            }
+        }
+        return sum;
+    }
+}
