5de7f2a59aac98e75adc5109cc1ed5b221315d07
NYgomets <126853427+NYgomets@users.noreply.github.com>
2025-01-25 19:11:13 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 85.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int result = 45;
+        for (int i : numbers) {
+            result -= i;
+        }
+        
+        return result;
+    }
+}
