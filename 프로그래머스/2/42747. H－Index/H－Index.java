e52f097172849f959d4e4408c080e3050c85a874
NYgomets <126853427+NYgomets@users.noreply.github.com>
2025-02-07 10:23:33 +0900

    [level 2] Title: H-Index, Time: 1.01 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -4,12 +4,12 @@ class Solution {
     public int solution(int[] citations) {
         Arrays.sort(citations);
         int length = citations.length;
+        int result = 0;
         for (int i = 0; i < length; i++) {
-            int hIndex = length - i;
-            if (citations[i] >= hIndex) {
-                return hIndex;
+            if (citations[i] >= length-i) {
+                result = Math.max(result, length-i);
             }
         }
-        return 0;
+        return result;
     }
 }
