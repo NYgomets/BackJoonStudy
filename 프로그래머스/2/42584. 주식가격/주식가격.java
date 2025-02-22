29fae439242cd1ab6286d168fd386a8b66ff8c0d
NYgomets <126853427+NYgomets@users.noreply.github.com>
2025-02-22 09:20:15 +0900

    [level 2] Title: 주식가격, Time: 40.94 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,18 +2,21 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
+        Stack<Integer> stack = new Stack<>();
         int[] answer = new int[prices.length];
         for (int i=0; i<prices.length; i++) {
-            int count = 0;
-            int current = prices[i];
-            for (int j=i+1; j<prices.length; j++) {
-                count++;
-                if (current > prices[j]) {
-                    break;
-                }
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                answer[stack.peek()] = i-stack.peek();
+                stack.pop();
             }
-            answer[i] = count;
+            stack.push(i);
         }
+        
+        while (!stack.isEmpty()) {
+            answer[stack.peek()] = prices.length - stack.peek() - 1;
+            stack.pop();
+        }
+        
         return answer;
     }
 }
