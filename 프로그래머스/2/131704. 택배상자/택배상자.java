f0eb95fd252bf7802a259b3705706ca97618c238
NYgomets <126853427+NYgomets@users.noreply.github.com>
2025-02-24 11:35:19 +0900

    [level 2] Title: 택배상자, Time: 46.67 ms, Memory: 156 MB -BaekjoonHub

---
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] order) {
+        Stack<Integer> stack = new Stack<>();
+        int answer = 0;
+        int current = 1;
+        for (int check : order) {
+            if (current == check) {
+                answer++;
+                current++;
+                continue;
+            }
+            
+            while (current <= check) {
+                stack.push(current);
+                current++;
+            }
+            
+            if (stack.peek() == check) {
+                answer++;
+                stack.pop();
+            } else {
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
