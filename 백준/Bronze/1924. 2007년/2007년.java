a1649ca6c81211315c7270948fdfe3241681903d
NYgomets <126853427+NYgomets@users.noreply.github.com>
2025-03-02 18:57:04 +0900

    [Bronze I] Title: 2007년, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/1924. 2007년/2007년.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] days = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+        int[] months = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int month = Integer.parseInt(st.nextToken());
+        int day = Integer.parseInt(st.nextToken());
+
+        int result = day;
+        for(int i = 0; i < month - 1; i++) {
+            result += months[i];
+        }
+        System.out.println(days[result % 7]);
+    }
+}
