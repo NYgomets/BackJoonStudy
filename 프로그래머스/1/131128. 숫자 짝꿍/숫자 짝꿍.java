3c1522ec43c1faafcd8797f632ea7b8a59ac03a1
NYgomets <126853427+NYgomets@users.noreply.github.com>
2025-03-12 23:27:18 +0900

    [level 1] Title: 숫자 짝꿍, Time: 60.97 ms, Memory: 131 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String X, String Y) {
+        int[] countX = new int[10];
+        int[] countY = new int[10];
+        for (int i=0; i<X.length(); i++) {
+            countX[X.charAt(i)-'0']++;
+        }
+        for (int i=0; i<Y.length(); i++) {
+            countY[Y.charAt(i)-'0']++;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i=9; i>=0; i--) {
+            int min = Math.min(countX[i], countY[i]);
+            for (int j=0; j<min; j++) {
+                sb.append(i);
+            }
+        }
+        
+        if (sb.length()==0) {
+            return "-1";
+        }
+        if (sb.charAt(0) == '0') {
+            return "0";
+        }
+        return sb.toString();
+    }
+}
