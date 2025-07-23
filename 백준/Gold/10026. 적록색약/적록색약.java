4e96b0d311e19c941c26ba446b5bee4cc65a5f33
NYgomets <126853427+NYgomets@users.noreply.github.com>
2025-07-23 22:41:29 +0900

    [Gold V] Title: 적록색약, Time: 148 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,95 +1,84 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
     static int n;
-    static String[][] maze;
-    static boolean[][] visitedArr;
-    static class Node {
-        int start;
-        int end;
-
-        public Node(int start, int end) {
-            this.start = start;
-            this.end = end;
-        }
-
-        public int getStart() {
-            return start;
-        }
-
-        public int getEnd() {
-            return end;
-        }
-    }
+    static Character[][] maze;
+    static boolean[][] visited;
+    static int[] directionX = {-1, 0, 1, 0};
+    static int[] directionY = {0, -1, 0, 1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        maze = new String[n][n];
+        maze = new Character[n][n];
+
         for (int i=0; i<n; i++) {
             String string = br.readLine();
             for (int j=0; j<n; j++) {
-                maze[i][j] = string.substring(j, j+1);
+                char c = string.charAt(j);
+                maze[i][j] = c;
             }
         }
 
-        visitedArr = new boolean[n][n];
-        System.out.print(check(false) + " ");
-        visitedArr = new boolean[n][n];
-        System.out.print(check(true));
+        visited = new boolean[n][n];
+        int result1 = check(false);
+        visited = new boolean[n][n];
+        int result2 = check(true);
+
+        System.out.println(result1 + " " + result2);
     }
 
     private static int check(boolean state) {
-        int area = 0;
+        int result = 0;
         for (int i=0; i<n; i++) {
             for (int j=0; j<n; j++) {
-                if (!visitedArr[i][j]) {
-                    dikstra(i, j, state);
-                    area++;
+                if (!visited[i][j]) {
+                    result++;
+                    bfs(i, j, maze[i][j], state);
                 }
             }
         }
-        return area;
+        return result;
     }
 
-    private static void dikstra(int startX, int startY, boolean state) {
-        Queue<Node> queue = new LinkedList<>();
-        queue.offer(new Node(startX, startY));
+    private static void bfs(int startX, int startY, Character check, boolean state) {
+        Queue<int[]> queue = new LinkedList<>();
+        visited[startX][startY] = true;
+        queue.add(new int[]{startX, startY});
 
         while (!queue.isEmpty()) {
-            Node poll = queue.poll();
-            int currentX = poll.getStart();
-            int currentY = poll.getEnd();
+            int[] poll = queue.poll();
+            int currentX = poll[0];
+            int currentY = poll[1];
 
-            if (visitedArr[currentX][currentY]) {
-                continue;
-            }
-            visitedArr[currentX][currentY] = true;
+            for (int i=0; i<4; i++) {
+                int nextX = currentX+directionX[i];
+                int nextY = currentY+directionY[i];
 
-            int[] directionX = {-1, 1, 0, 0};
-            int[] directionY = {0, 0, -1, 1};
-            for (int i = 0; i < 4; i++) {
-                int nextX = currentX + directionX[i];
-                int nextY = currentY + directionY[i];
-
-                if (nextX < 0 || nextX >= n || nextY < 0 || nextY >= n) {
+                if (nextX<0 || nextX>=n) {
+                    continue;
+                }
+                if (nextY<0 || nextY>=n) {
                     continue;
                 }
 
-                if (state) {
-                    if (!visitedArr[nextX][nextY]) {
-                        if (maze[currentX][currentY].equals(maze[nextX][nextY])) {
-                            queue.offer(new Node(nextX, nextY));
-                        } else if (maze[currentX][currentY].equals("R") || maze[currentX][currentY].equals("G")) {
-                            if (maze[nextX][nextY].equals("R") || maze[nextX][nextY].equals("G")) {
-                                queue.offer(new Node(nextX, nextY));
-                            }
-                        }
+                if (!state) {
+                    if (maze[nextX][nextY] == check && !visited[nextX][nextY]) {
+                        visited[nextX][nextY] = true;
+                        queue.add(new int[]{nextX, nextY});
                     }
                 } else {
-                    if (!visitedArr[nextX][nextY]) {
-                        if (maze[currentX][currentY].equals(maze[nextX][nextY])) {
-                            queue.offer(new Node(nextX, nextY));
+                    if (check == 'R' || check == 'G') {
+                        if ((maze[nextX][nextY] == 'R' || maze[nextX][nextY] == 'G') && !visited[nextX][nextY]) {
+                            visited[nextX][nextY] = true;
+                            queue.add(new int[]{nextX, nextY});
+                        }
+                    } else {
+                        if (maze[nextX][nextY] == check && !visited[nextX][nextY]) {
+                            visited[nextX][nextY] = true;
+                            queue.add(new int[]{nextX, nextY});
                         }
                     }
                 }
