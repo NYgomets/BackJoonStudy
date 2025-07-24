d80778b885ab33fa9c1e39a1b06d87960ba6876e
NYgomets <126853427+NYgomets@users.noreply.github.com>
2025-07-25 01:49:08 +0900

    [Silver I] Title: 숨바꼭질, Time: 148 ms, Memory: 23184 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,63 +1,48 @@
-import java.io.*;
-import java.math.BigInteger;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static class Node {
-        int distance;
-        int time;
-
-        public Node(int distance, int time) {
-            this.distance = distance;
-            this.time = time;
-        }
-
-        public int getDistance() {
-            return distance;
-        }
-
-        public int getTime() {
-            return time;
-        }
-    }
-    static int n;
-    static int k;
-    static boolean[] visitedArr;
+    static boolean[] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        visited= new boolean[100001];
 
-        visitedArr = new boolean[100001];
-        bfs(n);
-
+        int result = bfs(n, k);
+        System.out.println(result);
     }
 
-    private static void bfs(int start) {
-        Queue<Node> queue = new LinkedList<>();
-        visitedArr[start] = true;
-        queue.add(new Node(start, 0));
+    private static int bfs(int start, int end) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{start, 0});
+        visited[start] = true;
 
         while (!queue.isEmpty()) {
-            Node node = queue.poll();
-            int current = node.distance;
+            int[] poll = queue.poll();
+            int current = poll[0];
+            int time = poll[1];
 
-            if (current == k) {
-                System.out.println(node.getTime());
-                return;
+            if (current == end) {
+                return time;
             }
 
             int[] move = {-1, 1, current};
-
             for (int i=0; i<3; i++) {
-                int next = current + move[i];
+                int next = current+move[i];
 
-                if (next >=0 && next <= 100000 && !visitedArr[next]) {
-                    visitedArr[next] = true;
-                    queue.add(new Node(next, node.getTime()+1));
+                if ((0<=next && next<=100000) && !visited[next]) {
+                    visited[next] = true;
+                    queue.add(new int[]{next, time+1});
                 }
             }
         }
+
+        return 0;
     }
 }
