6fbafb296d376ba9056aaa7a8c285f631d6fdd7a
ParkJuhyeong <wngud5957@naver.com>
2026-04-01 19:48:22 +0900

    [Gold V] Title: 동전 2, Time: 160 ms, Memory: 36696 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -6,45 +6,48 @@ import java.util.*;
 public class Main {
     static int n;
     static int k;
-    static int[] coins;
     static Integer[][] dp;
+    static int[] coins;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
 
-        dp = new Integer[k+1][n];
         coins = new int[n];
-        for (int i=0; i<n; i++) {
-            coins[i] = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int c = Integer.parseInt(br.readLine());
+            coins[i] = c;
         }
 
-        int result = find(0, 0);
-        if (result == 10001) {
+        dp = new Integer[k+1][n];
+        int dfs = dfs(0, 0);
+        if  (dfs == 10001) {
             System.out.println(-1);
         } else {
-            System.out.println(result);
+            System.out.println(dfs);
         }
     }
 
-    private static int find(int current, int idx) {
-        if (current>k || idx>=n) {
+    private static int dfs(int curCoin, int index) {
+        if (curCoin > k || index >= n) {
             return 10001;
-        } else if (current == k) {
+        }
+
+        if (curCoin == k) {
             return 0;
         }
 
-        if (dp[current][idx] != null) {
-            return dp[current][idx];
+        if (dp[curCoin][index] != null) {
+            return dp[curCoin][index];
         }
-
+        
         int min = Integer.MAX_VALUE;
-        min = Math.min(min, find(current, idx+1));
-        min = Math.min(min, 1+find(current+coins[idx], idx));
+        min = Math.min(min, dfs(curCoin, index+1));
+        min = Math.min(min, 1+dfs(curCoin+coins[index], index));
 
-        dp[current][idx] = min;
-
+        dp[curCoin][index] = min;
+        
         return min;
     }
 }
