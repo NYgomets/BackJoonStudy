1996af1cdca268963ba3d1af08d2b8cf0dc98c7b
ParkJuhyeong <wngud5957@naver.com>
2026-04-01 12:21:27 +0900

    [Gold III] Title: 불!, Time: 1296 ms, Memory: 145748 KB -BaekjoonHub

---
+++ 백준/Gold/4179. 불！/불！.java
@@ -0,0 +1,110 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static String[][] maze;
+    static boolean[][] visited;
+    static int[] dx = {0, -1, 0, 1};
+    static int[] dy = {-1, 0, 1, 0};
+    static int n;
+    static int m;
+    static int pStartX;
+    static int pStartY;
+    static Queue<int[]> fire = new LinkedList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[n][m];
+        maze = new String[n][m];
+        for (int i=0; i<n; i++) {
+            String[] split = br.readLine().split("");
+            for (int j=0; j<m; j++) {
+                maze[i][j] = split[j];
+                if (split[j].equals("J")) {
+                    pStartY = i;
+                    pStartX = j;
+                } else if (split[j].equals("F")) {
+                    fire.add(new int[]{i,j});
+                }
+            }
+        }
+
+        int result = bfs();
+        if (result == -1) {
+            System.out.println("IMPOSSIBLE");
+        } else {
+            System.out.println(result);
+        }
+
+    }
+
+    private static int bfs() {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{pStartY, pStartX, 0});
+        int result = -1;
+
+        while (!queue.isEmpty()) {
+            fireBfs();
+            int size = queue.size();
+            for (int z = 0; z < size; z++) {
+                int[] poll = queue.poll();
+                int currentY = poll[0];
+                int currentX = poll[1];
+                int count = poll[2];
+
+                for (int i=0; i<4; i++) {
+                    int nextY = currentY + dy[i];
+                    int nextX = currentX + dx[i];
+
+                    if (nextY<0 || nextY>=n || nextX<0 || nextX>=m) {
+                        return count+1;
+                    }
+                    if (visited[nextY][nextX]) {
+                        continue;
+                    }
+                    if (maze[nextY][nextX].equals("#")) {
+                        continue;
+                    }
+
+                    visited[nextY][nextX] = true;
+                    queue.add(new int[]{nextY, nextX, count+1});
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private static void fireBfs() {
+        int size = fire.size();
+        for (int j=0; j<size; j++) {
+            int[] poll = fire.poll();
+            int currentY = poll[0];
+            int currentX = poll[1];
+            visited[currentY][currentX] = true;
+
+            for (int i=0; i<4; i++) {
+                int nextY = currentY + dy[i];
+                int nextX = currentX + dx[i];
+
+                if (nextY<0 || nextY>=n || nextX<0 || nextX>=m) {
+                    continue;
+                }
+                if (maze[nextY][nextX].equals("#")) {
+                    continue;
+                }
+                if (visited[nextY][nextX]) {
+                    continue;
+                }
+
+                fire.add(new int[]{nextY, nextX});
+                visited[nextY][nextX] = true;
+            }
+        }
+    }
+}
