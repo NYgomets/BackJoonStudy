a7c1026f6e2b50dde72f811711a19138da93ae8f
ParkJuhyeong <wngud5957@naver.com>
2026-04-08 16:54:54 +0900

    [Gold V] Title: 골목 대장 호석 - 기능성, Time: 104 ms, Memory: 14220 KB, Score: 18 point -BaekjoonHub

--- 백준/Gold/20168. 골목 대장 호석 － 기능성/골목 대장 호석 － 기능성.java
+++ 백준/Gold/20168. 골목 대장 호석 － 기능성/골목 대장 호석 － 기능성.java
@@ -6,18 +6,18 @@ import java.util.*;
 public class Main {
     static int n;
     static int m;
-    static int a;
-    static int b;
+    static int start;
+    static int end;
     static int c;
-    static List<Node>[] adjList;
     static boolean[] visited;
+    static List<Node>[] adjList;
     static class Node {
-        int end;
-        int weight;
+        int next;
+        int cost;
 
-        public Node(int end, int weight) {
-            this.end = end;
-            this.weight = weight;
+        public Node(int next, int cost) {
+            this.next = next;
+            this.cost = cost;
         }
     }
     static int result = Integer.MAX_VALUE;
@@ -26,45 +26,59 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        a = Integer.parseInt(st.nextToken());
-        b = Integer.parseInt(st.nextToken());
+        start = Integer.parseInt(st.nextToken());
+        end = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
 
+        visited = new boolean[n+1];
         adjList = new ArrayList[n+1];
-        for (int i=1; i<=n ;i++) {
+        for (int i=1; i<=n; i++) {
             adjList[i] = new ArrayList<>();
         }
 
-        for (int i=0; i<m; i++) {
+        for (int i=1; i<=m; i++) {
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-            adjList[start].add(new Node(end, cost));
-            adjList[end].add(new Node(start, cost));
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int z = Integer.parseInt(st.nextToken());
+            adjList[x].add(new Node(y, z));
+            adjList[y].add(new Node(x, z));
         }
 
-        visited = new boolean[n+1];
-        visited[a] = true;
-        dfs(a, 0, 0);
-        if (result != Integer.MAX_VALUE) {
-            System.out.println(result);
-        } else {
+        dikstra(start);
+        if (result == Integer.MAX_VALUE) {
             System.out.println(-1);
+        } else {
+            System.out.println(result);
         }
     }
 
-    private static void dfs(int start, int total, int max) {
-        if (start == b) {
-            result = Math.min(result, max);
-            return;
-        }
+    private static void dikstra(int start) {
+        Queue<int[]> queue = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+        queue.add(new int[]{start, 0, 0});
 
-        for (Node nodes : adjList[start]) {
-            if (!visited[nodes.end] && nodes.weight+total <= c) {
-                visited[nodes.end] = true;
-                dfs(nodes.end, nodes.weight+total, Math.max(max, nodes.weight));
-                visited[nodes.end] = false;
+        while (!queue.isEmpty()) {
+            int[] poll = queue.poll();
+            int current = poll[0];
+            int resultCost = poll[1];
+            int curTotal = poll[2];
+
+            if (current == end) {
+                result = Math.min(result, resultCost);
+            }
+
+            if (visited[current]) {
+                continue;
+            }
+            visited[current] = true;
+
+            for (Node node : adjList[current]) {
+                int next = node.next;
+                int cost = node.cost;
+
+                if (curTotal + cost <= c) {
+                    queue.add(new int[]{next, Math.max(resultCost, cost), curTotal+cost});
+                }
             }
         }
     }
