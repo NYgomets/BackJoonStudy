2fa7884f68ceebd2b812f80c1d285af2af8c3cc0
ParkJuhyeong <wngud5957@naver.com>
2026-04-09 12:26:46 +0900

    [Gold V] Title: 집합의 표현, Time: 916 ms, Memory: 58180 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,15 +1,17 @@
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
+    static int n;
+    static int m;
     static int[] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
         arr = new int[n+1];
         for (int i=1; i<=n; i++) {
             arr[i] = i;
@@ -18,12 +20,13 @@ public class Main {
         for (int i=0; i<m; i++) {
             st = new StringTokenizer(br.readLine());
             int check = Integer.parseInt(st.nextToken());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
             if (check == 0) {
-                union(start, end);
-            } else {
-                if (find(start) == find(end)) {
+                union(a, b);
+            }
+            if (check == 1) {
+                if (find(a) == find(b)) {
                     System.out.println("YES");
                 } else {
                     System.out.println("NO");
@@ -32,20 +35,7 @@ public class Main {
         }
     }
 
-    public static void union(int start, int end) {
-        int first = find(start);
-        int second = find(end);
-
-        if (first != second) {
-            if (first > second) {
-                arr[first] = second;
-            } else {
-                arr[second] = first;
-            }
-        }
-    }
-
-    public static int find(int num) {
+    private static int find(int num) {
         if (arr[num] != num) {
             arr[num] = find(arr[num]);
         }
@@ -53,5 +43,14 @@ public class Main {
         return arr[num];
     }
 
+    private static void union(int first, int second) {
+        int a = find(first);
+        int b = find(second);
 
+        if (a < b) {
+            arr[b] = a;
+        } else {
+            arr[a] = b;
+        }
+    }
 }
