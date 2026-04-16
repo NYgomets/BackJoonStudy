4f1f7c165abd5d7c77ceac37408f45ec7e682cb6
ParkJuhyeong <wngud5957@naver.com>
2026-04-16 10:54:59 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 92 ms, Memory: 14032 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.java
@@ -0,0 +1,8 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("2026");
+        System.out.println("04");
+        System.out.println("16");
+    }
+}
